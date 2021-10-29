715c68fcc92fb281ee416dc21666c959537f8161
yazgeldigithub <devster2016@gmail.com>
2021-10-28 21:23:03 -0500

    yaz

--- adjacentElementsProduct.js
+++ adjacentElementsProduct.js
@@ -1,9 +1,9 @@
 const adjacentElementsProduct = (inputArray) => {
   let highestNum = inputArray[0] * inputArray[1];
-  for (let i = 1; i < inputArray.length; i++) {
-    highestNum = Math.max(highestNum, inputArray[i - 1] * inputArray[i]);
+  for (let i = 0; i < inputArray.length - 1; i++) {
+    highestNum = Math.max(inputArray[i] * inputArray[i + 1]);
   }
   return highestNum;
 };
 
-console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3])); //21
